25c12b667f4e2e0613ec9ca0448b5c35a760c131
Noel Jacob <noeljacob91@gmail.com>
2023-07-14 16:27:23 +0530

    test run 12

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import {existsSync, mkdirSync, writeFileSync, readFileSync} from "fs";
 import {execSync} from "child_process";
 import {env, exit} from "process";
 
-if (!existsSync(`${env["HOME"]}/.ssh/aur_ed25519`)) {
-    if (!env["SSH_KEY"]) throw new Error("SSH_KEY not set");
-    if (!existsSync(`${env["HOME"]}/.ssh`)) mkdirSync(`${env["HOME"]}/.ssh`, {recursive: true});
-    writeFileSync(`${env["HOME"]}/.ssh/aur_ed25519`, env["SSH_KEY"], {encoding: "utf-8"});
-    execSync(`chmod 600 ${env["HOME"]}/.ssh/aur_ed25519`);
-}
+// if (!existsSync(`${env["HOME"]}/.ssh/aur_ed25519`)) {
+//     if (!env["SSH_KEY"]) throw new Error("SSH_KEY not set");
+//     if (!existsSync(`${env["HOME"]}/.ssh`)) mkdirSync(`${env["HOME"]}/.ssh`, {recursive: true});
+//     writeFileSync(`${env["HOME"]}/.ssh/aur_ed25519`, env["SSH_KEY"], {encoding: "utf-8"});
+//     execSync(`chmod 600 ${env["HOME"]}/.ssh/aur_ed25519`);
+// }
 const s = execSync("ssh-agent -s").toString() + `ssh-add ${env["HOME"]}/.ssh/aur_ed25519;\n`;
 execSync(s + "git submodule update --init --recursive");
 
