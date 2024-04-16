7d330bb1009d1a3665e48f846753711e2a096a14
Noel Jacob <noeljacob91@gmail.com>
2024-04-17 01:35:10 +0530

    stop workflow fail on silly errors

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ use regex::Regex;
 use data_encoding::HEXLOWER;
 use serde_json::Value;
 use structstruck::strike;
-use reqwest::Client;
+use reqwest::{Client};
 
 async fn get_aur_version(client: &Client, pkgs: Vec<&str>) -> Result<Vec<String>> {
-    let res = client
+    let res =  client
         .get(format!(
             "https://aur.archlinux.org/rpc/v5/info?arg[]={}",
             pkgs.join("&arg[]=")
@@ -87,7 +87,14 @@ async fn main() -> Result<()> {
         bun_bin::BunBin::new(&client).await?,
     ];
     let pkg_names = pkgs.iter().map(|p| p.pkg_name.as_str()).collect();
-    let aur_versions = get_aur_version(&client, pkg_names).await?;
+    let aur_versions = match get_aur_version(&client, pkg_names).await {
+        Ok(versions) => versions,
+        Err(e) if e.to_string().starts_with("error sending request for url") => {
+            println!("Warning: {}", e);
+            return Ok(());
+        },
+        Err(e) => return Err(e),
+    };
 
     let (pk,  k, mut repo_client) = setup_git_and_get_keys()?;
 
@@ -105,7 +112,14 @@ async fn main() -> Result<()> {
         let dir = repo.workdir()
             .ok_or_else(|| eyre!("No workdir"))?;
 
-        let replace_list = pkg.replace_list(&client).await?;
+        let replace_list = match pkg.replace_list(&client).await {
+            Ok(list) => list,
+            Err(e) if e.to_string() == "No checksums" => {
+                println!("Warning: {}", e);
+                continue;
+            },
+            Err(e) => return Err(e),
+        };
         for replace in replace_list {
             let mut file = fs::read_to_string(dir.join(&replace.filename))?;
             for (regex, value) in replace.regex {
